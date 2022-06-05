857dc26a9fd000d1bcdd99c6dcc66a1b8fd3f647
sezahedi <sezahedi13@gmail.com>
2022-06-05 22:22:43 +0430

    Add project files.

---
+++ Property_Example/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_Example
+{
+    internal class Car
+    {
+        private int _speed;
+        public int speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                if (value > 120)
+                {
+                    Console.WriteLine("Danger Drive slowly");
+                    _speed = 0;
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+    }
+}
---
+++ Property_Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car pride=new Car();
+            pride.speed = 120;
+
+            Console.WriteLine($"speed is {pride.speed}");
+            Console.ReadKey();
+        }
+    }
+}
